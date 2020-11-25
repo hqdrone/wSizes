90aa13816cdce6b22a9b1ad451bee003e1ab2a5f
Paul <hqdrone@gmail.com>
2020-11-25 11:33:25 +0300

    Update wSizes.js

--- dist/js/wSizes.js
+++ dist/js/wSizes.js
@@ -1,14 +1,14 @@
-window.onload = () => {
-	const wSizes = () => {
+window.onload = () => { // Страница загружена включая изображение, стили и тд
+	const wSizes = () => { // Объявляем основную функцию wSizes
 
-		const meta = document.createElement('meta')
-		meta.setAttribute('name', 'viewport')
-		meta.setAttribute('content', 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no"')
+		const meta = document.createElement('meta') // создаем элемент meta
+		meta.setAttribute('name', 'viewport') // добавляем элементу meta атрибут name="viewport"
+		meta.setAttribute('content', 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no"') // добавляем элементу meta атрибут для корректной работы плагина на мобильных устройствах
+		document.getElementsByTagName('head')[0].append(meta); // добавляем созданный элемент meta перед закрывающим тегом </head>
 
-		const badge = document.createElement('div')
-		document.getElementsByTagName('head')[0].append(meta);
-		badge.className = 'w-sizes'
-		badge.style.cssText = `
+		const wSize = document.createElement('div') // создаем элемент wSize
+		wSize.className = 'w-size' // добавляем класс w-size
+		wSize.style.cssText = `
 			padding: 5px;
 			background: #000;
 			position: fixed;
@@ -18,19 +18,17 @@ window.onload = () => {
 			color: #fff;
 			font-size: 12px;
 			border-radius: 3px;
-		`
-		document.body.append(badge)
+		` // добавляем инлайн стили
+		document.body.append(wSize) // добавляем созданный элемент meta перед закрывающим тегом </body>
 
-		const wResize = () => {
-			wWidth = document.documentElement.clientWidth
-			wHeight = document.documentElement.clientHeight
-			badge.innerText = `W: ${wWidth}px H: ${wHeight}px`
+		const wResize = () => { // объявляем функцию wResize
+			wWidth = document.documentElement.clientWidth // узнаем ширину окна без полосы прокрутки
+			wHeight = document.documentElement.clientHeight // узнаем высоту окна без полосы прокрутки
+			wSize.innerText = `W: ${wWidth}px H: ${wHeight}px` // вставляем значения ширины и высоты в элемент wSize
 		}
-		wResize()
-		window.addEventListener('resize', wResize)
+		wResize() // вызываем функцию wResize
+		window.addEventListener('resize', wResize) // вызывем функцию wResize при изменении размеров окна браузера
 	}
-	wSizes()
+
+	wSizes() // Вызываем основную функцию wSizes
 }
-
-
-
